496027349534874d4ead366da1c92c2267c7397b
leovct <l009.vincent@gmail.com>
2023-12-19 12:28:35 +0100

    chore: rename script

--- writer.sh
+++ writer.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-# Bash script to simulate the behavior of appending data to a file at regular intervals.
+# Bash script that simulates the behavior of appending data to a file at regular intervals.
 # For example, such program would write 1024 bytes each second to file.txt.
-# $ bash script.sh bytes 1024 1
+# $ bash writer.sh bytes 1024 1
 
 # The path of the file to which data will be appended.
-data_file_path="$1"
+file_path="$1"
 
 # The size of the data, in bytes, to write in each iteration.
-write_size="$2"
+write_size_bytes="$2"
 
 # The time interval, in seconds, between each write opeation in seconds.
-write_interval="$3"
+write_interval_seconds="$3"
 
 # Check if the correct number of arguments is provided.
 if [ "$#" -ne 3 ]; then
-  echo "Usage: $0 <file> <write_size_bytes> <write_interval_seconds>"
+  echo "Usage: $0 <file_path> <write_size_bytes> <write_interval_seconds>"
   exit 1
 fi
 
 # Store data to file given the parameters.
 while true; do
-  data=$(od -An -N $write_size /dev/urandom | tr -d ' \n')
-  echo "$data" >> "$data_file_path"
-  sleep "$write_interval"
+  data=$(od -An -N $write_size_bytes /dev/urandom | tr -d ' \n')
+  echo "$data" >> "$file_path"
+
+  sleep "$write_interval_seconds"
 done
